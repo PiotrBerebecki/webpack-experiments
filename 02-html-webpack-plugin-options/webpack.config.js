adacbd6da036d719f260469f930f2261f070a5c5
PiotrBerebecki <petebere@gmail.com>
2017-06-27 20:36:38 +0100

    Add production script

--- 02-html-webpack-plugin-options/webpack.config.js
+++ 02-html-webpack-plugin-options/webpack.config.js
@@ -10,8 +10,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
     ],
   },
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Notes:
 
 - Run `webpack --display-error-details` to error details
+- In experiment 02 `webpack -p` reduced the size of `bundle.js` from 18kB to 6kB
